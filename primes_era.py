b17b877cd3382c4ad872e9a9ea12e66a21cd870b
By698 <bysiek1998@gmail.com>
2019-10-16 22:08:01 +0200

    [Add] git ignore [Fix] primes_era.py with range input

--- primes_era.py
+++ primes_era.py
@@ -1,13 +1,14 @@
+range_ = int(input("Podaj górny zakres: "))
 prime_numbers = []
-for i in range(101):
+for i in range(range_ + 1):
     prime_numbers.append(True)
 number = 2
-while number ** 2 <= 100:
+while number ** 2 <= range_:
     if prime_numbers[number]:
-        for i in range(number * 2, 101, number):
+        for i in range(number * 2, range_ + 1, number):
             prime_numbers[i] = False
     number += 1
 
-for i in range(2, 101):
+for i in range(2, range_ + 1):
     if prime_numbers[i]:
         print(i)
