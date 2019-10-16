2900a1dcb848033591d0bc250b4d7242b4253d42
By698 <bysiek1998@gmail.com>
2019-10-16 22:19:17 +0200

    [Add, Fix] pi_wal.py add input and fix variable names

--- pi_wal.py
+++ pi_wal.py
@@ -1,5 +1,6 @@
+appr_numbers = int(input("Podaj ilość przybliżeń: "))
 i = 1
-for i in range(1, 11):
+for i in range(1, appr_numbers + 1):
     result = 1
     for n in range(1, i + 1):
         result *= 2*n * 2*n / (2*n - 1) / (2*n + 1)
