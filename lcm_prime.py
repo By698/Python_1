c3e83fcfc79178ae0f0019e970f328115b7b7273
By698 <bysiek1998@gmail.com>
2019-10-17 16:57:43 +0200

    .

--- lcm_prime.py
+++ lcm_prime.py
@@ -1,15 +1,4 @@
-a = int(input("Podaj pierwsza liczbe"))
-b = int(input("Podaj druga liczbe"))
+a = int(input("Podaj pierwsza liczbe: "))
+b = int(input("Podaj druga liczbe: "))
 
 
-def nwd(a, b):
-    if b > 0:
-        return nwd(b, a % b)
-    return a
-
-
-def nww(a, b):
-    return a*b//nwd(a, b)
-
-
-print(nww(a,b))
--- pi_wal.py
+++ pi_wal.py
@@ -1,7 +1,13 @@
-appr_numbers = int(input("Podaj ilość przybliżeń: "))
-i = 1
-for i in range(1, appr_numbers + 1):
-    result = 1
-    for n in range(1, i + 1):
-        result *= 2*n * 2*n / (2*n - 1) / (2*n + 1)
-    print(i, result * 2)
+
+def pi_wal(appr_numbers):
+    i = 1
+    for i in range(1, appr_numbers + 1):
+        result = 1
+        for n in range(1, i + 1):
+            result *= 2*n * 2*n / (2*n - 1) / (2*n + 1)
+        print(i, result * 2)
+
+
+if __name__ == '__main__':
+    appr_numbers = int(input("Podaj ilość przybliżeń: "))
+    pi_wal(appr_numbers)
